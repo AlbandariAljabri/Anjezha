8f84480df38cf0ac3ed757ec86344fe15f240b34
hadeel.i7 <hadeel.naji175@gmail.com>
2023-12-24 18:26:14 +0300

    department

--- Anjezha/service/urls.py
+++ Anjezha/service/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "accounts"
+app_name = "service"
 
 urlpatterns = [
-
+    path("add/departmrnt", views.add_department, name="add_department"),
+    path("display/department", views.display_department, name="display_department"),
+    path("department/details/<department_id>/", views.department_details, name="department_details"),
 ]

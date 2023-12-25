f3201b5fa26cee51ab1837a3e3b1a83ad7df9268
danahalsalmi <danahalmenjaf@gmail.com>
2023-12-25 14:00:37 +0300

    -

--- Anjezha/accounts/urls.py
+++ Anjezha/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", views.login_view, name="login_view"),
     path("logout/", views.logout_view, name="logout_view"),
-    path("profile/<user_id>/", views.user_profile_view, name="user_profile_view"),
+    path("profile/", views.user_profile_view, name="user_profile_view"),
 
 
 ]

ec33d4445304b988115efe8c624e1c256dc04e85
hadeel.i7 <hadeel.naji175@gmail.com>
2023-12-25 10:53:17 +0300

    department

--- Anjezha/service/urls.py
+++ Anjezha/service/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "service"
 
 urlpatterns = [
-    path("add/departmrnt", views.add_department, name="add_department"),
+    path("add/department", views.add_department, name="add_department"),
     path("display/department", views.display_department, name="display_department"),
-    path("department/details/<department_id>/", views.department_details, name="department_details"),
-    path("display/task/" , views.display_task_view , name="display_task_view"),
+    path("department/details/<department_id>/",
+         views.department_details, name="department_details"),
+    path("display/task/", views.display_task_view, name="display_task_view"),
 ]
--- Anjezha/service/views.py
+++ Anjezha/service/views.py
@@ -8,20 +8,23 @@ from accounts.models import Department
 def add_department(request: HttpRequest):
     if request.method == "POST":
         new_department = Department(title=request.POST["title"], description=request.POST["description"],
-                         Image=request.FILES["Image"])
+                                    Image=request.FILES["Image"])
         new_department.save()
         return redirect("service:display_department")
-    return render("service/add_department.html")
+    return render(request, "service/add_department.html")
+
 
 def display_department(request: HttpRequest):
-    
+
     department = Department.objects.all()
     return render(request, "service/display_department.html", {"departments": department})
+
 
 def department_details(request: HttpRequest, department_id):
 
     department_detail = Department.objects.get(id=department_id)
-    return render(request,"service/department_details.html", {"department": department_detail})
+    return render(request, "service/department_details.html", {"department": department_detail})
 
-def display_task_view(request : HttpRequest):
-    return render(request , "service/display_task.html")
+
+def display_task_view(request: HttpRequest):
+    return render(request, "service/display_task.html")

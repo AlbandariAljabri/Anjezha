b8bf3f892ac7dc1bf9fbefdbb2fc72af4b820eff
hadeel.i7 <hadeel.naji175@gmail.com>
2023-12-26 11:52:03 +0300

    models

--- Anjezha/accounts/models.py
+++ Anjezha/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from service.models import Task
 # from django_countries.fields import CountryField
 
 
@@ -9,16 +10,19 @@ from django.contrib.auth.models import User
 class Department(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    Image = models.ImageField(upload_to="img/" , default="img/logo.png")
+    Image = models.ImageField(upload_to="img/", default="img/logo.png")
+    supervisor = models.OneToOneField(
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name='supervised_department')
+    worker = models.ManyToManyField(User, related_name='working_departments')
+    task = models.ForeignKey(Task, related_name='assigned_tasks', on_delete=models.CASCADE)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField()
-    # nationality = CountryField()   
-    avatar = models.ImageField(upload_to="img/" , default="img/avatar.png")
-    department = models.ForeignKey(Department , on_delete=models.CASCADE )
-    
-    def __str__(self):
-      return f"{self.nationality.name} - {self.user.first_name}" 
+    # nationality = CountryField()
+    avatar = models.ImageField(upload_to="img/", default="img/avatar.png")
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.nationality.name} - {self.user.first_name}"

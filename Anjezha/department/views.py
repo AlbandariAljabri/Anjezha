2573219e910ab5ab1f928c854e123dd6232d156c
hadeel.i7 <hadeel.naji175@gmail.com>
2023-12-28 14:46:48 +0300

    department add & delete button

--- Anjezha/department/views.py
+++ Anjezha/department/views.py
@@ -13,14 +13,14 @@ def add_department(request: HttpRequest):
     if request.method == "POST":
         new_department = Department(title=request.POST["title"], description=request.POST["description"],image=request.FILES["image"])
         new_department.save()
-        return redirect("service:display_department")
-    return render(request, "service/add_department.html")
+        return redirect("department:display_department")
+    return render(request, "department/add_department.html")
 
 
 def display_department(request: HttpRequest):
 
     department = Department.objects.all()
-    return render(request, "service/display_department.html", {"department": department})
+    return render(request, "department/display_department.html", {"department": department})
 
 
 def department_details(request: HttpRequest, department_id):
@@ -29,7 +29,7 @@ def department_details(request: HttpRequest, department_id):
     available_supervisors = Profile.objects.exclude(user=department.supervisor)
     available_worker = Profile.objects.exclude(user__in=department.worker.all())
 
-    return render(request, "service/department_details.html", {"department": department, 'available_supervisors': available_supervisors, 'available_worker': available_worker})
+    return render(request, "department/department_details.html", {"department": department, 'available_supervisors': available_supervisors, 'available_worker': available_worker})
 
 
 def update_department(request: HttpRequest, department_id):
@@ -41,15 +41,15 @@ def update_department(request: HttpRequest, department_id):
         department.image = request.FILES["image"]
         department.save()
 
-        return redirect("service:display_department")
-    return render(request, "service/update_department.html", {"department": department})
+        return redirect("department:display_department")
+    return render(request, "department/update_department.html", {"department": department})
 
 
 def delete_department(request: HttpRequest, department_id):
 
     department = Department.objects.get(id=department_id)
     department.delete()
-    return redirect("service:display_department")
+    return redirect("department:display_department")
 
 
 def add_department_worker(request: HttpRequest, department_id, worker_id):
@@ -57,7 +57,7 @@ def add_department_worker(request: HttpRequest, department_id, worker_id):
     worker = Profile.objects.get(id=worker_id)
     department.workers.add(worker.user)
 
-    return redirect("service:department_details", department_id=department_id)
+    return redirect("department:department_details", department_id=department_id)
 
 
 def remove_department_worker(request: HttpRequest, department_id, worker_id):
@@ -68,7 +68,7 @@ def remove_department_worker(request: HttpRequest, department_id, worker_id):
     worker = Profile.objects.get(id=worker_id)
     department.worker.remove(worker.user)
 
-    return redirect("service:department_details", department_id=department_id)
+    return redirect("department:department_details", department_id=department_id)
 
 
 def add_department_supervisor(request: HttpRequest, department_id, supervisor_id):
@@ -77,7 +77,7 @@ def add_department_supervisor(request: HttpRequest, department_id, supervisor_id
     department.supervisor = supervisor.user
     department.save()
 
-    return redirect("service:department_details", department_id=department_id)
+    return redirect("department:department_details", department_id=department_id)
 
 def replace_department_supervisor(request, department_id, supervisor_id):
     department = Department.objects.get(id=department_id)
@@ -88,5 +88,5 @@ def replace_department_supervisor(request, department_id, supervisor_id):
 
     department.supervisor = new_supervisor.user
     department.save() #Set the new supervisor
-    return redirect("service:department_details", department_id=department_id)
+    return redirect("department:department_details", department_id=department_id)
 

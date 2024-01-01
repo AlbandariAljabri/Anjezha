a1edad4ccf5925b677ba21a5c3f0ab20e8aa62e6
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-01-01 12:27:25 +0300

    css style

--- Anjezha/contact/urls.py
+++ Anjezha/contact/urls.py
@@ -5,6 +5,8 @@ app_name = "contact"
 
 urlpatterns = [
     path("contact/", views.contact_view, name="contact_view"),
-    path("confirmtaion/", views.thank_you_view, name="thank_you_view"),
+    path("confirmation/", views.thank_you_view, name="thank_you_view"),
+    path('message/',views.message_view,name='message_view'),
+
 
 ]

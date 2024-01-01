466c0707eafb17b41a3db2d1a45917367247d23d
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-01-01 20:04:54 +0300

    display messages & Update Status

--- Anjezha/contact/urls.py
+++ Anjezha/contact/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path("contact/", views.contact_view, name="contact_view"),
     path("confirmation/", views.thank_you_view, name="thank_you_view"),
     path('message/',views.message_view,name='message_view'),
+    path('display/<message_id>',views.display_message_view,name="display_message_view"),
+    path('update/<message_id>',views.status_view,name="status_view"),
+
+
 
 
 ]
--- Anjezha/contact/views.py
+++ Anjezha/contact/views.py
@@ -28,6 +28,27 @@ def message_view(request:HttpRequest,):
 
     return render(request, "contact/messages.html", {"message" :message})
 
+def display_message_view(request:HttpRequest ,message_id):
+    message = Contact.objects.get(id=message_id)
+    
+    return render(request, "contact/display_message.html", {"message" : message })
+
+def status_view(request:HttpRequest,message_id):
+
+    if not request.user.is_staff:
+        return render(request, "main/not_authorized.html", status=401)
+    
+    message = Contact.objects.get(id=message_id)
+
+    if request.method == "POST":
+
+        message.status = request.POST["status"]
+        message.save()
+    
+        return redirect('contact:display_message_view' ,message_id)
+    
+    return render(request, 'contact/update_status.html',{"message": message ,"status_choices":Contact.status_choices})
+
 def thank_you_view(request:HttpRequest):
     return render(request , "contact/thank_you.html")
 
